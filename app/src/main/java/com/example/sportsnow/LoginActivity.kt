cc26974bdcac39a2e304d1e55a9d0e3db2956a17
JEduardoMorato23 <200510021@ucc.mx>
2023-12-04 23:27:41 -0600

    "Vistas y clases para login y register (variables nombradas)"

--- app/src/main/java/com/example/sportsnow/LoginActivity.kt
+++ app/src/main/java/com/example/sportsnow/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.sportsnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val etEmailLogin : EditText = findViewById(R.id.etEmailLogin)
+        val etPasswordLogin : EditText = findViewById(R.id.etPasswordLogin)
+        val btIngresar : Button = findViewById(R.id.btIngresar)
+        val btResPassword : Button = findViewById(R.id.btResPassword)
 
 
     }
--- app/src/main/java/com/example/sportsnow/RegisterActivity.kt
+++ app/src/main/java/com/example/sportsnow/RegisterActivity.kt
@@ -1,13 +1,35 @@
 package com.example.sportsnow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
 
+        val etNombreUsuario : EditText =  findViewById(R.id.etNombreUsuarioRegistro)
+        val etApellidoP : EditText = findViewById(R.id.etApellidoPaternoRegistro)
+        val etApellidoM : EditText = findViewById(R.id.etApellidoMaternoRegistro)
+        val etEmail : EditText = findViewById(R.id.etEmailRegistro)
+        val etPassword : EditText = findViewById(R.id.etPasswordRegistro)
+        val etConfirmPassword : EditText = findViewById(R.id.etConfirmPasswordRegistro)
+        val etDate : EditText = findViewById(R.id.etDateRegistro)
+        val btCancel : Button = findViewById(R.id.btCancelRegister)
+        val btSave : Button = findViewById(R.id.btSaveRegister)
+
+        btCancel.setOnClickListener {
+            finish()
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
+        btSave.setOnClickListener {
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
